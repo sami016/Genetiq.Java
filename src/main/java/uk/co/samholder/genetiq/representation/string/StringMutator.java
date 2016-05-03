9fbe7d4917c9eadaac6a748052aa673a7a94c40e
Sam Holder <samholder@live.co.uk>
2016-05-03 23:31:44 +0100

    Moved string related classes into strings representation folder.

--- src/main/java/uk/co/samholder/genetiq/representation/string/StringMutator.java
+++ src/main/java/uk/co/samholder/genetiq/representation/string/StringMutator.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package uk.co.samholder.genetiq.mutator.string;
+package uk.co.samholder.genetiq.representation.string;
 
 import java.util.Random;
 import uk.co.samholder.genetiq.mutator.Mutator;
--- src/main/java/uk/co/samholder/genetiq/representation/string/StringUniformCrossover.java
+++ src/main/java/uk/co/samholder/genetiq/representation/string/StringUniformCrossover.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package uk.co.samholder.genetiq.combiner.string;
+package uk.co.samholder.genetiq.representation.string;
 
 import java.util.List;
 import java.util.Random;
