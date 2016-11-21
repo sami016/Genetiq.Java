bd59f2c04e9fee705c009102fb5f2bf5fc255408
sami016 <samholder@live.co.uk>
2016-11-21 21:05:45 +0000

    added uniform vector crossover.

---
+++ src/main/java/uk/co/samholder/genetiq/representation/vector/UniformVectorCrossover.java
@@ -0,0 +1,39 @@
+package uk.co.samholder.genetiq.representation.vector;
+
+import java.util.List;
+import java.util.Random;
+import uk.co.samholder.genetiq.combiner.Combiner;
+
+/**
+ *
+ * @author Sam Holder
+ */
+public class UniformVectorCrossover implements Combiner<Vector> {
+
+    private Random random;
+
+    public UniformVectorCrossover(Random random) {
+        this.random = random;
+    }
+    
+    @Override
+    public int getNumberPerCrossover() {
+        return 2;
+    }
+
+    private float selectAtLoci(List<Vector> individuals, int loci) {
+        return random.nextBoolean() 
+                ? individuals.get(0).getValue(loci)
+                : individuals.get(1).getValue(loci);
+    }
+    
+    @Override
+    public Vector combine(List<Vector> individuals) {
+        Vector vec = new Vector(individuals.get(0).getDimensions());
+        for (int i=0; i<vec.getDimensions(); i++) {
+            vec.setValue(i, selectAtLoci(individuals, i));
+        }
+        return vec;
+    }
+    
+}
